efdacb12ede796128782440e024ac9babf91763d
paulo.murer <paulo.murer@infinity6.ai>
2026-05-10 13:09:24 +0000

    site release

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,7 @@
     $(window).ready(async () => {
         const params = new URLSearchParams(location.search);
         const shop = params.get('shop')
-        const themeUrl = (shop ? `https://${shopDomain}/admin/themes/current/editor` : "shopify://admin/themes" )
+        const themeUrl = (shop ? `https://${shop}/admin/themes/current/editor` : "shopify://admin/themes" )
         $('.theme-link').attr('href', themeUrl)
     })
 
